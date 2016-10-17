79e6c23b9f9edefa190bb8e52dd51683cd83d6fb
bibarz <ibarz@braincorporation.com>
2016-10-17 09:02:11 -0700

    cleanup

--- sotana_flask.py
+++ sotana_flask.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import subprocess
 import os
+import time
 from sotana import read_words_and_kanji, ChartParser, jReads, tokenizedRomaji, ref_string, sep_string
 from flask import Flask, session, redirect, url_for, render_template
 
@@ -9,302 +9,19 @@ from flask import Flask, session, redirect, url_for, render_template
 app = Flask(__name__)
 app.secret_key = 'sotana'
 histories = {}
+session_expiration_seconds = 24 * 3600 * 7  # sessions expire after not being used for this
 os.environ['PATH'] += ':/usr/local/bin'
 
 
-class Card(object):
-    def __init__(self, n_frames, app, frame, **params):
-        self.app = app
-        self.card_frame = frame
-        self.frames = [Tk.Frame(frame, **params) for _ in range(n_frames)]
-        self.blinders = [Tk.Frame(frame, **params) for _ in self.frames]
-        for i, b in enumerate(self.blinders):
-            b.bind("<Button-1>", lambda event, i=i: self.turn(i))
-            
-        for i, (f, b) in enumerate(zip(self.frames, self.blinders)):
-            b.grid(row=i)
-            f.grid(in_=b)
-
-        self.visible = [False for _ in self.frames]
-
-    def turn(self, i):
-        if self.visible[i]:
-            self.frames[i].lower(self.blinders[i])
-        else:
-            self.frames[i].lift(self.blinders[i])
-        self.visible[i] = not self.visible[i]
-
-    def reveal_more(self):
-        for i, v in enumerate(self.visible):
-            if not v:
-                self.turn(i)
-                return
-        # All is revealed; hide last
-        self.turn(-1)
-
-
-class WordCard(Card):
-    def __init__(self, data, *args, **kwargs):
-        Card.__init__(self, 3, *args, **kwargs)
-
-        format = {'fg': 'white',
-                  'bg': 'black'}
-
-        l = Tk.Label(self.frames[0], text=data['meaning'], font=("Lucida Grande", 20), **format)
-        l.bind("<Button-1>", lambda event: self.turn(0))
-        l.grid(row=0)
-        
-        kana = ''.join([x.split(',')[-1] for x in jReads(data['word'])])
-        romaji = ' '.join(tokenizedRomaji(unicode(data['word']))).encode('utf-8')
-        for col, c in enumerate(data['word']):
-            l = Tk.Label(self.frames[2], text=c, font=("Lucida Grande", 60), **format)
-            if 0x4e00 <= ord(c) <= 0x9faf:  # kanji
-                l.bind("<Button-1>", lambda event, c=c: self.app.show_kanji(c))
-            else:
-                l.bind("<Button-1>", lambda event: self.turn(2))
-            l.grid(row=0, column=col)
-
-        l = Tk.Label(self.frames[1], text=kana, font=("Lucida Grande", 20), **format)
-        l.bind("<Button-1>", lambda event: self.turn(1))
-        l.grid(row=0)
-        l = Tk.Label(self.frames[1], text=romaji, font=("Lucida Grande", 20), **format)
-        l.bind("<Button-1>", lambda event: self.turn(1))
-        l.grid(row=1)
-        l = Tk.Label(self.frames[1], text=ref_string(data), font=("Lucida Grande", 20), **format)
-        l.bind("<Button-1>", lambda event: self.turn(1))
-        l.grid(row=2)
-
-        if data['related words']:
-            related_frame = Tk.Frame(self.frames[1], bg='black')
-            words = sep_string(data['related words'])
-            for i, w in enumerate(words):
-                w = w.strip()
-                for k in self.app.words.keys():
-                    if w in self.app.words[k]:
-                        elem = Tk.Button(related_frame, text=w, font=("Lucida", 20),
-                                         command=lambda i=(self.app.words[k].index(w), k): self.app.show_next(i),
-                                         **format)
-                        break
-                else:
-                    elem = Tk.Label(related_frame, text=w, font=("Lucida", 20), **format)
-                elem.grid(row=0, column=i)
-            related_frame.grid(row=3)
-
-        if 'example' in data and data['example']:
-            example_frame = Tk.Frame(self.frames[2], bg='black')
-            l = Tk.Text(example_frame, font=("Lucida Grande", 14), width=30,
-                        height=len(data['example'])/30 + 1, **format)
-            l.insert('1.0', data['example'])
-            l.grid(row=0)
-            example_frame.grid(row=1, columnspan=len(data['word']))
-
-
-class KanjiCard(Card):
-    def __init__(self, data, *args, **kwargs):
-        Card.__init__(self, 6, *args, **kwargs)
-
-        format = {'fg': 'white',
-                  'bg': 'black'}
-
-        l = Tk.Label(self.frames[0], text=data['kanji'], font=("Lucida Grande", 60), **format)
-        l.bind("<Button-1>", lambda event: browse_kanji(data['kanji']))
-        l.grid(row=0)
-        
-        for col, c in enumerate(sep_string(data['kun'])):
-            l = Tk.Label(self.frames[1], text=c, font=("Lucida Grande", 20), **format)
-            l.bind("<Button-1>", lambda event, c=c: self.app.kanjis_with_kun(c))
-            l.grid(row=0, column=col)
-        for col, c in enumerate(sep_string(data['on'])):
-            l = Tk.Label(self.frames[2], text=c, font=("Lucida Grande", 20), **format)
-            l.bind("<Button-1>", lambda event, c=c: self.app.kanjis_with_on(c))
-            l.grid(row=0, column=col)
-        for col, c in enumerate(sep_string(data['related kanji'])):
-            l = Tk.Label(self.frames[3], text=c, font=("Lucida Grande", 40), **format)
-            if 0x4e00 <= ord(c) <= 0x9faf:  # kanji
-                l.bind("<Button-1>", lambda event, c=c: self.app.show_kanji(c))
-            l.grid(row=0, column=col)
-
-        l = Tk.Text(self.frames[4], font=("Lucida Grande", 28), width=len(data['meaning']), height=1, **format)
-        l.insert('1.0', data['meaning'])
-        l.bind("<Button-1>", lambda event, c=c: self.app.words_from_kanji(data['kanji']))
-        l.grid(row=0)
-        l = Tk.Text(self.frames[5], font=("Lucida Grande", 14), width=30, height=len(data['explanation']) / 30 + 1, **format)
-        l.insert('1.0', data['explanation'])
-        l.grid(row=0)
-
-        for i in range(1, 6):
-            self.turn(i)
-
-
-class App():
-    def __init__(self, dict_data, kanji_data, default_dict_name, master=None, **params):
-        Tk.Frame.__init__(self, master, **params)
-        self.grid(sticky=Tk.N + Tk.S + Tk.E + Tk.W)  # make it resizable
-        self.dict_data = dict_data
-        self.default_dict_name = default_dict_name
-        self.kanji_data = kanji_data
-        self.words = {k: [d['word'] for d in dict_data[k]] for k in dict_data.keys()}
-        self.kanji = [d['kanji'] for d in kanji_data]
-        self.history = []
-        self.hist_index = 0
-        self.probs = np.ones(len(self.dict_data[self.default_dict_name])) / len(self.dict_data[self.default_dict_name])
-        
-        btn = Tk.Button(self, text='New', command=self.show_next)
-        btn.grid(row=0, column=0)
-        self.prev_btn = Tk.Button(self, text='Prev', command=self.history_back)
-        self.prev_btn.grid(row=0, column=1)
-        self.next_btn = Tk.Button(self, text='Next', command=self.history_forward)
-        self.next_btn.grid(row=0, column=2)
-        self.idx_label = Tk.Label(self, text='', font=("Lucida Grande", 14), fg='white', bg='black')
-        self.idx_label.grid(row=0, column=3)
-        self.card_frame = Tk.Frame(self, **params)
-        self.card_frame.grid(row=1, columnspan=4)
-        self.kanji_frame = None
-        self.kanji_list_frame = None
-        self.word_list_frame = None
-        self.bind_all("<Right>", self.next_or_forward)
-        self.bind_all("<Left>", self.nothing_or_backward)
-        self.bind_all("<space>", self.reveal_more)
-        
-        self.show_next()
-        self.grid()
-        
-    def reveal_more(self, event):
-        self.last_word_card.reveal_more()
-
-    def next_or_forward(self, event):
-        if self.next_btn['state'] == 'disabled':
-            self.show_next()
-        else:
-            self.history_forward()
-
-    def nothing_or_backward(self, event):
-        if self.prev_btn['state'] == 'disabled':
-            return
-        else:
-            self.history_back()
-
-    def add_to_history(self, i):
-        self.history = self.history[:self.hist_index] + [i]
-        self.hist_index += 1
-        self._update_hist_button_status()
-
-    def history_back(self):
-        self.hist_index -= 1
-        self.show_next(i=self.history[self.hist_index-1], remember=False)
-        self._update_hist_button_status()
-
-    def history_forward(self):
-        self.show_next(i=self.history[self.hist_index], remember=False)
-        self.hist_index += 1
-        self._update_hist_button_status()
-
-    def _update_hist_button_status(self):
-        self.prev_btn['state'] = 'disabled' if self.hist_index == 1 else 'normal'
-        self.next_btn['state'] = 'disabled' if self.hist_index >= len(self.history) else 'normal'
-        self.idx_label['text'] = str("%i/%i" % (self.hist_index, len(self.history)))
-
-    def random_sample(self):
-        i = np.digitize(np.random.random(1), np.cumsum(self.probs))[0]
-        self.probs[i] *= 0.2
-        self.probs /= np.sum(self.probs)
-        return i
-
-    def show_kanji(self, c):
-        if c not in self.kanji:
-            browse_kanji(c)
-            return
-
-        i = self.kanji.index(c)
-
-        if self.kanji_frame is None:
-            self.kanji_frame = Tk.Toplevel(self, bg='black')
-            self.kanji_frame.geometry('+400+0')
-            self.kanji_frame.protocol("WM_DELETE_WINDOW", self.kanji_frame_closed)
-
-        for w in self.kanji_frame.winfo_children():
-            if isinstance(w, Tk.Frame):
-                w.destroy()
-
-        self.last_kanji_card = KanjiCard(self.kanji_data[i], self, self.kanji_frame, bg='black')
-        self.last_kanji_card.turn(0)
-    
-    def kanji_frame_closed(self):
-        self.kanji_frame.destroy()
-        self.kanji_frame = None
-
-    def show_kanji_list(self, kanji_list, **format):
-        if self.kanji_list_frame is None:
-            self.kanji_list_frame = Tk.Toplevel(self, bg='black')
-            self.kanji_list_frame.geometry('+100+400')
-            self.kanji_list_frame.protocol("WM_DELETE_WINDOW", self.kanji_list_frame_closed)
-
-        for w in self.kanji_list_frame.winfo_children():
-            if isinstance(w, Tk.Widget):
-                w.destroy()
-
-        format = {'fg': 'white',
-                  'bg': 'black'}
-
-        for i, c in enumerate(kanji_list):
-            l = Tk.Label(self.kanji_list_frame, text=c, font=("Lucida Grande", 60), **format)
-            l.bind("<Button-1>", lambda event, c=c: self.show_kanji(c))
-            l.grid(row=0, column=i)
-
-    def kanji_list_frame_closed(self):
-        self.kanji_list_frame.destroy()
-        self.kanji_list_frame = None
-
-    def kanjis_with_kun(self, c):
-        k_list = []
-        for d in self.kanji_data:
-            kuns = sep_string(d['kun'])
-            if c in kuns:
-                k_list.append(d['kanji'])
-        self.show_kanji_list(k_list)
-        
-    def kanjis_with_on(self, c):
-        k_list = []
-        for d in self.kanji_data:
-            ons = sep_string(d['on'])
-            if c in ons:
-                k_list.append(d['kanji'])
-        self.show_kanji_list(k_list)
-
-    def show_word_list(self, word_index_list, **format):
-        if self.word_list_frame is None:
-            self.word_list_frame = Tk.Toplevel(self, bg='black')
-            self.word_list_frame.geometry('+100+400')
-            self.word_list_frame.protocol("WM_DELETE_WINDOW", self.word_list_frame_closed)
-
-        for w in self.word_list_frame.winfo_children():
-            if isinstance(w, Tk.Widget):
-                w.destroy()
-
-        format = {'fg': 'white',
-                  'bg': 'black'}
-
-        for i, index in enumerate(word_index_list):
-            idx, key = (index, self.default_dict_name) if isinstance(index, (float, int)) else index
-            l = Tk.Label(self.word_list_frame, text=self.dict_data[key][idx]['word'], font=("Lucida Grande", 40), **format)
-            l.bind("<Button-1>", lambda event, index=index: self.show_next(index))
-            l.grid(row=0, column=i)
-
-    def word_list_frame_closed(self):
-        self.word_list_frame.destroy()
-        self.word_list_frame = None
-
-    def words_from_kanji(self, c):
-        word_index_list = []
-        for key in self.dict_data.keys():
-            for i, d in enumerate(self.dict_data[key]):
-                if c in d['word']:
-                    word_index_list.append((i, key))
-        self.show_word_list(word_index_list)
-        
+def update_time_decorator(f):
+    def wrapper(self, *args, **kwargs):
+        self.last_time_used = time.time()
+        return f(self, *args, **kwargs)
+    return wrapper
 
 class History(object):
     def __init__(self):
+        self.last_time_used = time.time()
         self.history = []
         self.hist_index = 0
         self.current_kanji = None
@@ -321,11 +38,15 @@ class History(object):
         self.probs /= np.sum(self.probs)
         return i
 
+    @update_time_decorator
     def add_to_history(self, i):
+        self.last_used_time = time.time()
         self.history = self.history[:self.hist_index] + [i]
         self.hist_index += 1
 
+    @update_time_decorator
     def hist_button_status(self):
+        self.last_used_time = time.time()
         status = {
             'has_prev': self.hist_index > 1,
             'has_next': self.hist_index < len(self.history),
@@ -333,14 +54,24 @@ class History(object):
         }
         return status
 
+    @update_time_decorator
     def history_back(self):
         self.hist_index -= 1
         self.show_next(i=self.history[self.hist_index-1], remember=False)
 
+    @update_time_decorator
     def history_forward(self):
         self.show_next(i=self.history[self.hist_index], remember=False)
         self.hist_index += 1
         self._update_hist_button_status()
+
+
+def _cleanup_histories():
+    # Delete old histories after session_expiration_seconds inactive
+    t = time.time()
+    for c in histories.keys():
+        if t - histories[c].last_time_used > session_expiration_seconds:
+            del histories[c]
 
 
 def _get_session_history():
@@ -355,11 +86,12 @@ def _get_session_history():
 @app.route('/')
 def default():
     return '''
-    <a href='/start_session'> Start! </a>
+    <a href='/start_session'> Click to start </a>
     '''
 
 @app.route('/start_session')
 def start_session():
+    _cleanup_histories()
     code = np.random.random()
     session['code'] = code
     histories[code] = History()
@@ -483,4 +215,4 @@ def next_card():
 
 @app.route('/no_session')
 def no_session():
-    return ("No session!")
+    return ("Session expired!")
